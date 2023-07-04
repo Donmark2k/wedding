037b6a457072316e04474d5feb228843b2e0c94a
Donmark2k <chukwuemekavp@gmail.com>
2023-07-04 20:23:45 +0100

    Add the Gallery component

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,10 +1,203 @@
+import Carousel from 'react-bootstrap/Carousel';
+
 const Gallery = () => {
   
   return (
-    <header>
-      <h1>todos</h1>
-      <p>Items will persist in the browser local storage</p>
-    </header>
+    // our gallery
+    <div
+      class="container-fluid bg-gallery"
+      id="gallery"
+      style="padding: 120px 0; margin: 90px 0"
+    >
+      <div
+        class="section-title position-relative text-center"
+        style="margin-bottom: 120px"
+      >
+        <h6
+          class="text-uppercase mb-3"
+          style="letter-spacing: 3px; color: #e47a2e !important"
+        >
+          Gallery
+        </h6>
+        <h1 class="font-secondary display-4 text-white">Our Photo Gallery</h1>
+        <i class="far fa-heart text-white"></i>
+      </div>
+      {/* <!-- carousel slider  */}
+      <div class="owl-carousel gallery-carousel owl-loaded owl-drag">
+        <div class="owl-stage-outer">
+          <div
+            class="owl-stage"
+            style="
+              transform: translate3d(-954px, 0px, 0px);
+              transition: all 0s ease 0s;
+              width: 3820px;
+            "
+          >
+            <div class="owl-item cloned" style="width: 318.333px">
+              <div class="gallery-item">
+                <img
+                  class="img-fluid w-100"
+                  src="https://fandg23.fidelcom.org/storage/albums/MtcEHobaeIKId5cdeAESHgRLjDw31xgII7RZ1mk8.jpg"
+                  alt=""
+                />
+                <a
+                  href="https://fandg23.fidelcom.org/storage/albums/MtcEHobaeIKId5cdeAESHgRLjDw31xgII7RZ1mk8.jpg"
+                  data-lightbox="gallery"
+                >
+                  <i class="fa fa-2x fa-plus text-white"></i>
+                </a>
+              </div>
+            </div>
+            <div class="owl-item cloned" style="width: 318.333px">
+              <div class="gallery-item">
+                <img
+                  class="img-fluid w-100"
+                  src="https://fandg23.fidelcom.org/storage/albums/eughhNpdtPaAHOKWZRbdXyKgh2THSTKZAzA7Zle4.jpg"
+                  alt=""
+                />
+                <a
+                  href="https://fandg23.fidelcom.org/storage/albums/eughhNpdtPaAHOKWZRbdXyKgh2THSTKZAzA7Zle4.jpg"
+                  data-lightbox="gallery"
+                >
+                  <i class="fa fa-2x fa-plus text-white"></i>
+                </a>
+              </div>
+            </div>
+            <div class="owl-item cloned" style="width: 318.333px">
+              <div class="gallery-item">
+                <img
+                  class="img-fluid w-100"
+                  src="https://fandg23.fidelcom.org/storage/albums/fhx9jmjGuZQyA9SH1O881VNe8M9DScg1okcY7YsF.jpg"
+                  alt=""
+                />
+                <a
+                  href="https://fandg23.fidelcom.org/storage/albums/fhx9jmjGuZQyA9SH1O881VNe8M9DScg1okcY7YsF.jpg"
+                  data-lightbox="gallery"
+                >
+                  <i class="fa fa-2x fa-plus text-white"></i>
+                </a>
+              </div>
+            </div>
+            <div class="owl-item active" style="width: 318.333px">
+              <div wire:id="uUc4q8w6TUnvn1DNBKpS" class="gallery-item">
+                <img
+                  class="img-fluid w-100"
+                  src="https://fandg23.fidelcom.org/storage/albums/wSoqjEpefIsRCu7eCRxefeajnlKl2ClRQlEQbDAh.jpg"
+                  alt=""
+                />
+                <a
+                  href="https://fandg23.fidelcom.org/storage/albums/wSoqjEpefIsRCu7eCRxefeajnlKl2ClRQlEQbDAh.jpg"
+                  data-lightbox="gallery"
+                >
+                  <i class="fa fa-2x fa-plus text-white"></i>
+                </a>
+              </div>
+            </div>
+            <div class="owl-item active" style="width: 318.333px">
+              <div class="gallery-item">
+                <img
+                  class="img-fluid w-100"
+                  src="https://fandg23.fidelcom.org/storage/albums/SDgFVbuG4prZiz5WZtLJUPnDjpoWilSVLhtjsWCG.jpg"
+                  alt=""
+                />
+                <a
+                  href="https://fandg23.fidelcom.org/storage/albums/SDgFVbuG4prZiz5WZtLJUPnDjpoWilSVLhtjsWCG.jpg"
+                  data-lightbox="gallery"
+                >
+                  <i class="fa fa-2x fa-plus text-white"></i>
+                </a>
+              </div>
+            </div>
+            <div class="owl-item active" style="width: 318.333px">
+              <div class="gallery-item">
+                <img
+                  class="img-fluid w-100"
+                  src="https://fandg23.fidelcom.org/storage/albums/GnwxYQfn0LThj1psMJZwo5Cvl263V1Lk6l2geErL.jpg"
+                  alt=""
+                />
+                <a
+                  href="https://fandg23.fidelcom.org/storage/albums/GnwxYQfn0LThj1psMJZwo5Cvl263V1Lk6l2geErL.jpg"
+                  data-lightbox="gallery"
+                >
+                  <i class="fa fa-2x fa-plus text-white"></i>
+                </a>
+              </div>
+            </div>
+            <div class="owl-item" style="width: 318.333px">
+              <div class="gallery-item">
+                <img
+                  class="img-fluid w-100"
+                  src="https://fandg23.fidelcom.org/storage/albums/eughhNpdtPaAHOKWZRbdXyKgh2THSTKZAzA7Zle4.jpg"
+                  alt=""
+                />
+                <a
+                  href="https://fandg23.fidelcom.org/storage/albums/eughhNpdtPaAHOKWZRbdXyKgh2THSTKZAzA7Zle4.jpg"
+                  data-lightbox="gallery"
+                >
+                  <i class="fa fa-2x fa-plus text-white"></i>
+                </a>
+              </div>
+            </div>
+            <div class="owl-item" style="width: 318.333px">
+              <div class="gallery-item">
+                <img
+                  class="img-fluid w-100"
+                  src="https://fandg23.fidelcom.org/storage/albums/wSoqjEpefIsRCu7eCRxefeajnlKl2ClRQlEQbDAh.jpg"
+                  alt=""
+                />
+                <a
+                  href="https://fandg23.fidelcom.org/storage/albums/wSoqjEpefIsRCu7eCRxefeajnlKl2ClRQlEQbDAh.jpg"
+                  data-lightbox="gallery"
+                >
+                  <i class="fa fa-2x fa-plus text-white"></i>
+                </a>
+              </div>
+            </div>
+
+            <div class="owl-item cloned" style="width: 318.333px">
+              <div wire:id="uUc4q8w6TUnvn1DNBKpS" class="gallery-item">
+                <img
+                  class="img-fluid w-100"
+                  src="https://fandg23.fidelcom.org/storage/albums/SDgFVbuG4prZiz5WZtLJUPnDjpoWilSVLhtjsWCG.jpg"
+                  alt=""
+                />
+                <a
+                  href="https://fandg23.fidelcom.org/storage/albums/SDgFVbuG4prZiz5WZtLJUPnDjpoWilSVLhtjsWCG.jpg"
+                  data-lightbox="gallery"
+                >
+                  <i class="fa fa-2x fa-plus text-white"></i>
+                </a>
+              </div>
+            </div>
+
+            <div class="owl-item cloned" style="width: 318.333px">
+              <div class="gallery-item">
+                <img
+                  class="img-fluid w-100"
+                  src="https://fandg23.fidelcom.org/storage/albums/GnwxYQfn0LThj1psMJZwo5Cvl263V1Lk6l2geErL.jpg"
+                  alt=""
+                />
+                <a
+                  href="https://fandg23.fidelcom.org/storage/albums/GnwxYQfn0LThj1psMJZwo5Cvl263V1Lk6l2geErL.jpg"
+                  data-lightbox="gallery"
+                >
+                  <i class="fa fa-2x fa-plus text-white"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="owl-nav">
+          <div class="owl-prev">
+            <i class="fa fa-angle-left" aria-hidden="true"></i>
+          </div>
+          <div class="owl-next">
+            <i class="fa fa-angle-right" aria-hidden="true"></i>
+          </div>
+        </div>
+        <div class="owl-dots disabled"></div>
+      </div>
+    </div>
   );
 };
 export default Gallery;
