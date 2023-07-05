b29f1f622249093706e6bf231b55d10a3571415e
Donmark2k <chukwuemekavp@gmail.com>
2023-07-05 18:09:48 +0100

    Style the Wedding component

--- src/components/event.js
+++ src/components/event.js
@@ -2,10 +2,66 @@
 const Event = () => {
   
     return (
-      <header>
-        <h1>Event</h1>
-        <p>Items will persist in the browser local storage</p>
-      </header>
+      // Wedding Event
+      <div class="container-fluid py-5" id="event">
+      <div class="container py-5">
+        <div class="section-title position-relative text-center">
+          <h6
+            class="text-uppercase mb-3"
+            style="letter-spacing: 3px; color: #e47a2e !important"
+          >
+            Event
+          </h6>
+          <h1 class="font-secondary display-4">Our Wedding Event</h1>
+          <i class="far fa-heart text-dark"></i>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-6 text-center">
+            <h5 class="font-weight-normal text-muted mb-3 pb-3">
+              We cannot wait to spend our afternoon with you as we celebrate the
+              love we share.
+            </h5>
+          </div>
+        </div>
+        <div class="row">
+          <div
+            class="col-md-6 border-right"
+            style="border-color: #e47a2e !important"
+          >
+            <div class="text-center text-md-right mr-md-3 mb-4 mb-md-0">
+              <img
+                class="img-fluid mb-4"
+                src="	https://fandg23.fidelcom.org/storage/albums/GnwxYQfn0LThj1psMJZwo5Cvl263V1Lk6l2geErL.jpg"
+                alt
+              />
+              <h4 class="mb-3">The Reception</h4>
+              <p class="mb-2">
+                CWO Hall, Our Lady of Assumption Catholic Church, Okwuani,
+                Nnewi, Anambra State.
+              </p>
+              <p class="mb-0">1:00PM - 4:00PM</p>
+              <p class="mb-0">20 May, 2023</p>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="text-center text-md-left ml-md-3">
+              <img
+                class="img-fluid mb-4"
+                src="https://fandg23.fidelcom.org/img/event-2.jpg"
+                alt
+              />
+              <h4 class="mb-3">Wedding Party</h4>
+              <p class="mb-2">
+                HOUSE ON THE ROCK, NNEWI. 150 IGWE ORIZU RD, OTOLO, NNEWI,
+                ANAMBRA STATE.
+              </p>
+              <p class="mb-0">10:00AM - 12:00PM</p>
+              <p class="mb-0">20 May, 2023</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     );
   };
   export default Event;
