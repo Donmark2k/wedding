e4d76ad3f345d2549a9bc634da15707124081d5d
Donmark2k <chukwuemekavp@gmail.com>
2023-07-15 21:31:32 +0100

    Style each card

--- src/components/gift.js
+++ src/components/gift.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import covidMImage from '../asset/gift/covidM.png';
+import leaderboardMImage from '../asset/gift/leaderboardM.png';
+import '../App.css';
+
 
 const Gift = () => {
   const projects = [
     {
       id: '1',
       name: 'Covid-19 Countries\' Statistics',
-      homeImage: 'src/asset/gift/covidM.png',
+      homeImage: covidMImage,
       description: 'Covid-19 Countries\' Statistics',
-      technologies: ['redux', 'Bootstrap', 'reactJS'],
       modalInfo: 'A COVID-19 statistics app is a mobile application designed to provide up-to-date information on the number of confirmed cases, deaths, and recoveries from the COVID-19 pandemic. The app can help users track the current situation of the pandemic in their country or region, and also provide information on the trends and patterns of the virus globally.',
       liveLink: 'https://covidst.netlify.app/',
       sourceLink: 'https://github.com/Donmark2k/covid-stat',
@@ -15,9 +18,17 @@ const Gift = () => {
     {
       id: '2',
       name: 'Leaderboard',
-      homeImage: 'src/asset/gift/leaderboardM.png',
+      homeImage: leaderboardMImage,
       description: 'Leaderboard',
-      technologies: ['Javascript', 'css', 'html'],
+      modalInfo: 'A leaderboard is a feature commonly found in competitive online games and applications, including chess, that displays the rankings of players based on their scores or other performance metrics.',
+      liveLink: 'https://donmark2k.github.io/leaderboard/dist/',
+      sourceLink: 'https://github.com/Donmark2k/leaderboard',
+    },
+    {
+      id: '3',
+      name: 'Leaderboard',
+      homeImage: leaderboardMImage,
+      description: 'Leaderboard',
       modalInfo: 'A leaderboard is a feature commonly found in competitive online games and applications, including chess, that displays the rankings of players based on their scores or other performance metrics.',
       liveLink: 'https://donmark2k.github.io/leaderboard/dist/',
       sourceLink: 'https://github.com/Donmark2k/leaderboard',
@@ -28,19 +39,15 @@ const Gift = () => {
   return (
     <section id="myrecentwork" className="grid-container" aria-label="my recent work">
       {projects.map((project) => (
-        <article key={project.id} className="firstbox">
+        <article key={project.id} className="articleBox">
           <div className="recentImage">
             <img className="articlePic" src={project.homeImage} alt="image details" />
           </div>
           <p>{project.description}</p>
-          <ul aria-label="Languages used in coding the work">
-            {project.technologies.map((technology) => (
-              <li key={technology}>{technology}</li>
-            ))}
-          </ul>
+          
           <div className="button-box">
             <a href={project.liveLink} className="see-more buttons" aria-label="view">
-              See Project
+              Gift us
             </a>
           </div>
         </article>
