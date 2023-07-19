b261bf845e6021201e2408919b695164f59707a3
Donmark2k <chukwuemekavp@gmail.com>
2023-07-19 14:54:44 +0100

    Make the footer responsive

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,35 +1,16 @@
-import { Container, Row, Col } from 'react-bootstrap';
-import { FaTwitter, FaFacebookF, FaLinkedinIn, FaInstagram, FaEnvelope, FaHeart } from 'react-icons/fa';
+import { Container } from 'react-bootstrap';
 
 const Footer = () => {
   return (
-    <div className="container-fluid text-white py-5" style={{ backgroundColor: '#121f38' }} id="contact">
-      <Container className="text-center py-5">
+    <div className="container-fluid text-white py-2" style={{ backgroundColor: '#121f38' }} id="contact">
+      <Container className="text-center py-0">
         <div className="section-title position-relative text-center">
-          <h1 className="font-secondary display-3 text-white">Thank You</h1>
-          <div className="heart-container">
-  <hr className="line" />
-  <FaHeart className="heart-icon" />
-  <hr className="line" />
-</div>        </div>
-        <div className="d-flex justify-content-center mb-4">
-          <a className="btn btn-lg btn-outline-light btn-lg-square mr-2" href="#"><FaTwitter /></a>
-          <a className="btn btn-lg btn-outline-light btn-lg-square mr-2" href="#"><FaFacebookF /></a>
-          <a className="btn btn-lg btn-outline-light btn-lg-square mr-2" href="#"><FaLinkedinIn /></a>
-          <a className="btn btn-lg btn-outline-light btn-lg-square" href="#"><FaInstagram /></a>
-        </div>
-        <div className="d-flex justify-content-center py-2">
-          <p className="text-white">
-            <a href="#" style={{ color: '#e47a2e' }} data-cfemail="#">
-              <FaEnvelope className="mr-1" />
-              chukwuemekavp@gmail.com
-            </a>
-          </p>
-          <span className="px-3">|</span>
-          <p className="text-white">+234 706 330 8955</p>
-        </div>
-        <p className="m-0">
-          &copy; <a style={{ color: '#e47a2e' }} href="#">R&J2023</a>. Designed by{' '}
+          <h1 className="font-secondary display-3 text-white" style={{ color: '#e47a2e', fontSize: '2rem' }} >Thank You for Coming 😍 </h1>
+               </div>
+           
+        <p className="m-0 pb-2">
+          &copy; <a style={{ color: '#e47a2e', fontSize: '24px' }} href="#">R&J2023</a>. </p> 
+          <p>Designed by{' '}
           <a style={{ color: '#e47a2e' }} href="https://chukwuemekavp.netlify.app/">
             CHUKWUEMEKAVP ENTERPRISE
           </a>
