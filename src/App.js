59f648179a6ad4bc63218692d3bd723ddf679e1a
Donmark2k <chukwuemekavp@gmail.com>
2023-07-25 08:47:22 +0100

    Make gift responsive

--- src/App.js
+++ src/App.js
@@ -7,15 +7,18 @@ import Event from './components/event';
 import Story from './components/story';
 import Slide from './components/slide';
 import Footer from './components/footer';
-import Gift from './components/gift';
+// import Gift from './components/gift';
 // import Media from './components/media';
+import Present from './components/present';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Routes>
-        <Route path="/gifts" element={<Gift />} />
+        {/* <Route path="/gifts" element={<Gift />} /> */}
+        <Route path="/gifts" element={<Present />} />
+
       </Routes>
       <Slide />
       <Story />
