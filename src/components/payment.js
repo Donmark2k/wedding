6bb041361c18f283a45073aacae079d4b13f7c4e
Donmark2k <chukwuemekavp@gmail.com>
2023-09-30 18:51:23 +0100

    Add payment details

--- src/components/payment.js
+++ src/components/payment.js
@@ -1,57 +1,59 @@
-import React from 'react';
-// import PaystackPop from 'paystack-pop';
-// import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { useState } from 'react';
+import PaystackPop from '@paystack/inline-js';
 
-const Payment = () => {
-  function payWithPaystack(e) {
+const PaystackIntegration = () => {
+  const [email, setEmail] = useState('');
+  const [amount, setAmount] = useState('');
+  const [firstname, setFirstname] = useState('');
+  const [lastname, setLastname] = useState('');
+
+  const paywithpaystack = (e) => {
     e.preventDefault();
-
-    // const handler = PaystackPop.setup({
-    //   key: 'pk_test_86963e6c82576f869d86c89ae441bec44073e913', // Replace with your public key
-    //   email: document.getElementById('email-address').value,
-    //   amount: document.getElementById('amount').value * 100,
-    //   ref: `${Math.floor((Math.random() * 1000000000) + 1)}`,
-    //   onClose() {
-    //     toast.error('Payment window closed.', { autoClose: 3000 });
-    //   },
-    //   callback(response) {
-    //     const message = `Payment complete! Reference: ${response.reference}`;
-    //     toast.success(message, { autoClose: 5000 });
-    //   },
-    // });
-
-    // handler.openIframe();
-  }
-
+    const paystack = new PaystackPop();
+    paystack.newTransaction({
+      key: 'pk_test_86963e6c82576f869d86c89ae441bec44073e913',
+      amount: amount * 100,
+      email,
+      firstname,
+      lastname,
+      onSuccess(transaction) {
+        const message = `Payment Complete! Reference ${transaction.reference}`;
+        alert(message);
+        setEmail('');
+        setAmount('');
+        setFirstname('');
+        setLastname('');
+      },
+      onCancel() {
+        alert('You have cancelled the transaction');
+      },
+    });
+  };
   return (
     <>
       <form id="paymentForm">
         <div className="form-group">
           <label htmlFor="email-address">Email Address</label>
-          <input type="email" id="email-address" required />
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} id="email-address" required />
         </div>
         <div className="form-group">
           <label htmlFor="amount">Amount</label>
-          <input type="tel" id="amount" required />
+          <input type="tel" value={amount} onChange={(e) => setAmount(e.target.value)} id="amount" required />
         </div>
         <div className="form-group">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" />
+          <input type="text" value={firstname} onChange={(e) => setFirstname(e.target.value)} id="first-name" />
         </div>
         <div className="form-group">
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" />
+          <input type="text" value={lastname} onChange={(e) => setLastname(e.target.value)} id="last-name" />
         </div>
         <div className="form-submit">
-          <button type="submit" onClick={payWithPaystack}> Pay </button>
+          <button type="submit" onClick={paywithpaystack}> Pay </button>
         </div>
       </form>
-      {/* <ToastContainer /> */}
-      {' '}
-      {/* Container for displaying toasts */}
     </>
   );
 };
 
-export default Payment;
+export default PaystackIntegration;
